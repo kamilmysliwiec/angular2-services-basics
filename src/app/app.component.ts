dd0ea6001fac644f8069e606d832d17433db31ec
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2017-01-22 22:13:15 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,12 @@
-import { Component, ViewChild, ElementRef, ViewChildren, QueryList } from '@angular/core';
+import { Component } from '@angular/core';
 import { Todo } from "./todo";
-import { AddTodoComponent } from "./add-todo/add-todo.component";
-import { TodoItemComponent } from "./todo-item/todo-item.component";
-import { TodosService } from "./todos.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ TodosService ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('ref') ref: ElementRef;
-  @ViewChildren(TodoItemComponent) todosRefsList: QueryList<TodoItemComponent>;
-
   public todos: Todo[];
 
   constructor(private todosService: TodosService) {
